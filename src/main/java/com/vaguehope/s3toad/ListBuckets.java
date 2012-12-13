27cb72e5a91c6b99ceee03da55b1de2a0966a076
はく <haku@vaguehope.com>
2012-12-13 14:48:36 +0000

    Tidy list output.

--- src/main/java/com/vaguehope/s3toad/ListBuckets.java
+++ src/main/java/com/vaguehope/s3toad/ListBuckets.java
@@ -1,7 +1,5 @@
 package com.vaguehope.s3toad;
 
-import java.util.List;
-
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
 
@@ -14,8 +12,8 @@ public class ListBuckets {
 	}
 
 	public void run() {
-		List<Bucket> list = this.s3Client.listBuckets();
-		for (Bucket b : list) {
+		for (Bucket b : this.s3Client.listBuckets()) {
+			System.err.print("bucket=");
 			System.err.println(b.getName());
 		}
 	}
