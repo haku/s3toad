b6ec79c95b5cf1f820afd5b28ec3869e4a4b42e4
はく <haku@vaguehope.com>
2012-12-18 21:39:23 +0000

    Always clean up transfer manager.

--- src/main/java/com/vaguehope/s3toad/DownloadSimple.java
+++ src/main/java/com/vaguehope/s3toad/DownloadSimple.java
@@ -15,30 +15,31 @@ public class DownloadSimple {
 	private final String bucket;
 	private final String key;
 
-	public DownloadSimple(AmazonS3 s3Client, String bucket, String key) {
+	public DownloadSimple (AmazonS3 s3Client, String bucket, String key) {
 		this.s3Client = s3Client;
 		this.bucket = bucket;
 		this.key = key;
 	}
 
-	public void run() throws AmazonClientException, InterruptedException {
+	public void run () throws AmazonClientException, InterruptedException {
 		File localFile = new File(new File(this.key).getName());
 		System.err.println("localFile=" + localFile.getAbsolutePath());
-
 		TransferManager tm = new TransferManager(this.s3Client);
-		PrgTracker tracker = new PrgTracker();
-		GetObjectRequest request = new GetObjectRequest(this.bucket, this.key)
-				.withProgressListener(tracker);
-
-		final long startTime = System.currentTimeMillis();
-		Download download = tm.download(request, localFile);
-		System.err.println("contentLength=" + download.getObjectMetadata().getContentLength());
-		download.waitForCompletion();
-
-		tracker.print();
-		System.err.println("duration=" + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime) + "s");
-
-		tm.shutdownNow();
+		try {
+			PrgTracker tracker = new PrgTracker();
+			final long startTime = System.currentTimeMillis();
+			Download download = tm.download(
+					new GetObjectRequest(this.bucket, this.key)
+							.withProgressListener(tracker),
+					localFile);
+			System.err.println("contentLength=" + download.getObjectMetadata().getContentLength());
+			download.waitForCompletion();
+			tracker.print();
+			System.err.println("duration=" + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime) + "s");
+		}
+		finally {
+			tm.shutdownNow();
+		}
 	}
 
 }
